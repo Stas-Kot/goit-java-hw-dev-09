856ee4bb7b9f3b350f1712af7fc2a373564885c7
Stas Kotelnykov <staskotelnykov@gmail.com>
2022-12-16 18:37:28 +0200

    thymeleaf add

--- src/main/java/com/goit/feature/ThymeleafController.java
+++ src/main/java/com/goit/feature/ThymeleafController.java
@@ -7,6 +7,7 @@ import org.thymeleaf.templateresolver.FileTemplateResolver;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -38,14 +39,15 @@ public class ThymeleafController extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
-
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         ZoneId zid = ZoneId.of(timeZoneUtil.parseTimeZone(req));
         Clock clock = Clock.system(zid);
         initTime = LocalDateTime.now(clock).format(DateTimeFormatter.ofPattern(
                 "yyyy-MM-dd hh:mm:ss "
         )) + zid;
+
+        resp.setContentType("text/html");
+        resp.addCookie(new Cookie("lastTimezone", zid.toString()));
 
         Context simpleContext = new Context(
                 req.getLocale(),
--- src/main/java/com/goit/feature/util/TimeZoneUtil.java
+++ src/main/java/com/goit/feature/util/TimeZoneUtil.java
@@ -1,13 +1,22 @@
 package com.goit.feature.util;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.stream.Stream;
 
 public class TimeZoneUtil {
     public String parseTimeZone(HttpServletRequest request) {
+        String standardTZ = "UTC";
+        String result = (request.getCookies() == null) ? standardTZ :
+                Stream.of(request.getCookies())
+                        .filter(c -> "lastTimezone".equals(c.getName()))
+                        .map(Cookie::getValue)
+                        .findAny().orElseGet(() -> standardTZ);
+
         if (request.getParameterMap().containsKey("timezone")) {
-            return (request.getParameter("timezone").length() < 1) ?
-                    "UTC" : request.getParameter("timezone").replace(" ", "+").toUpperCase();
+            return (request.getParameter("timezone").replace(" ", "+").length() < 1) ?
+                    result : request.getParameter("timezone").replace(" ", "+").toUpperCase();
         }
-        return "UTC";
+        return result;
     }
 }
